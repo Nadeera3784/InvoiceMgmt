2cb814c9b7f1ea2cb328caf95830fe7ca1d6506d
sage <tuzzy08@gmail.com>
2018-03-15 17:14:15 -0400

    Modified app.js, models/invoice.js

--- app.js
+++ app.js
@@ -1,59 +1,62 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var expresshbs = require('express-handlebars');
-var mongoose = require('mongoose');
-var validator = require('express-validator');
-var session = require('express-session');
-var passport = require('passport');
-var flash = require('connect-flash');
-var autoIncrement = require('mongoose-auto-increment');
+const express = require('express');
+
+const path = require('path');
+
+// const favicon = require('serve-favicon');
+
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expresshbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const validator = require('express-validator');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const autoIncrement = require('mongoose-auto-increment');
 
 // Make database connection
 mongoose.connect('mongodb://localhost:27017/charm');
-var db = mongoose.connection;
+const db = mongoose.connection;
 
 autoIncrement.initialize(db);
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', () => {
   // we're connected!
-  console.log("Database Connection Successful");
+  console.log('Database Connection Successful');
 });
 
 require('./config/passport');
 
-
-
-var app = express();
+const app = express();
 
 // view engine setup
-app.engine('.hbs', expresshbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.engine('.hbs', expresshbs({ defaultLayout: 'layout', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({saveUninitialized: false, resave: false, secret: 'xaxbczczaaxx#213._+@__..000xxx'}))
+app.use(session({ saveUninitialized: false, resave: false, secret: 'xaxbczczaaxx#213._+@__..000xxx' }));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.locals.login = req.isAuthenticated();
   next();
 });
 
 // Set up CORS
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Allow-Headers', 'content-type, accept');
@@ -62,9 +65,9 @@ app.use(function(req, res, next) {
 });
 
 // app.use('/create', create);
-var create = require('./routes/create');
-var user = require('./routes/user');
-var index = require('./routes/index');
+const create = require('./routes/create');
+const user = require('./routes/user');
+const index = require('./routes/index');
 
 
 app.use('/', index);
@@ -72,14 +75,14 @@ app.use('/user', user);
 app.use('/create', create);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -92,40 +95,17 @@ app.use(function(err, req, res, next) {
 // module.exports = app;
 
 
+const debug = require('debug')('charm:server');
 
+const http = require('http');
 
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-var debug = require('debug')('charm:server');
-var http = require('http');
-
-/**
- * Get port from environment and store in Express.
- */
-
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
@@ -149,7 +129,7 @@ function onError(error) {
     throw error;
   }
 
-  var bind = typeof port === 'string'
+  const bind = typeof port === 'string'
     ? 'Pipe ' + port
     : 'Port ' + port;
 
@@ -173,12 +153,38 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
+  const addr = server.address();
+  const bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
 }
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const appPort = normalizePort(process.env.PORT || '3000');
+
+app.set('port', appPort);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(appPort);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
 
 
 
--- models/invoice.js
+++ models/invoice.js
@@ -1,27 +1,26 @@
 const mongoose = require('mongoose');
-var autoIncrement = require('mongoose-auto-increment');
-const Schema = mongoose.Schema;
+
+const autoIncrement = require('mongoose-auto-increment');
+
+const Schema  = mongoose.Schema;
 
 // Sub Documents
 const itemSchema = new Schema({
-    // index: 'string',
-    value: 'string'
+  value: 'string',
 });
 
 const schema = new Schema({
-    name: 'String',
-    // invoiceID: Number,
-    created_by: 'String',
-    // deliverer: 'String',
-    subTotal: 'String',
-    grandTotal: 'String',
-    vat: 'String',
-    description: [itemSchema],
-    quantity: [itemSchema],
-    remarks: [itemSchema],
-    cost: [itemSchema],
-    date: 'String',
-    created_at: {type: Date, default: Date.now}
+  name: 'String',
+  created_by: 'String',
+  subTotal: 'String',
+  grandTotal: 'String',
+  vat: 'String',
+  description: [itemSchema],
+  quantity: [itemSchema],
+  remarks: [itemSchema],
+  cost: [itemSchema],
+  date: 'String',
+  created_at: { type: Date, default: Date.now },
 });
 
 schema.plugin(autoIncrement.plugin, { model: 'Invoice', field: 'invoiceId', startAt: 1 });
