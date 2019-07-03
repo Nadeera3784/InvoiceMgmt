49acb933c53985cf4ff387bb5b8855cc91084c9f
Sage <tuzzy08@gmail.com>
2019-07-03 09:01:00 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const router = express.Router();
-// const csurf = require('csurf');
 
 const passport = require('passport');
 const mongoose = require('mongoose');
